650a4aae1d30b8d9b6d44d5ca9af20e240229380
Muhammad Nur Faza <gofar222@gmail.com>
2023-06-20 10:40:15 +0700

    first commit

---
+++ tugas27.js
@@ -0,0 +1,15 @@
+//1
+let string1 = "No ";
+let string2 = "rumah saya ";
+let string3 = " A";
+let int = 30;
+let intString1 = String(int);
+let alamat1 = string1 + string2 + intString1 + string3;
+console.log(alamat1);
+
+//2
+let jalan = "Jalan Pangeran Tirtayasa No ";
+let int2 = 70;
+let instring2 = String(int2);
+let alamat2 = jalan + instring2;
+console.log(alamat2);
